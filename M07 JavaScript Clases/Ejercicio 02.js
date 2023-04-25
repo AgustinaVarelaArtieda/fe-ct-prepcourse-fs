e63ead0a0e6736ba87bce52fb41364fdd6b56c02
agusv <agusvarela5@gmail.com>
2023-04-25 19:54:19 -0300

    Recordar como y cuando usar new y prototype

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,6 +6,21 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+   constructor(nombre, apellido, edad, domicilio){
+      this.nombre=nombre;
+      this.apellido=apellido;
+      this.edad=edad;
+      this.domicilio=domicilio;
+   }
+   detalle(){
+      var propPersona = {
+      nombre:this.nombre,
+      apellido:this.apellido,
+      edad:this.edad,
+      domicilio:this.domicilio
+      }
+      return propPersona
+   }
 }
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
@@ -13,6 +28,9 @@ function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+
+   var nuevaPersona= new Persona (nombre,apellido,edad,domicilio)
+   return nuevaPersona
 }
 
 function agregarMetodo() {
@@ -20,7 +38,12 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   Persona.prototype.datos= function(){
+      return this.nombre+', '+this.edad+' años'
+   }
 }
+
+//LISTO
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
